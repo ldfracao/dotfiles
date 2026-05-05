c476c21506920caa3663470f32ddeb63e0590fd6
Leandro Fração <ldfracao@protonmail.com>
2026-05-05 14:07:47 -0300

    working jump to mark function

--- .config/nvim/lua/modules/helper_functions.lua
+++ .config/nvim/lua/modules/helper_functions.lua
@@ -1,24 +1,27 @@
 local M = {}
 
+local special_marks = {
+    ["'"] = true, ['"'] = true, ["^"] = true, ["."] = true,
+    ["["] = true, ["]"] = true, ["<"] = true, [">"] = true
+}
+
 function M.is_valid_mark(char)
-	if #char ~= 1 then return false end
-	-- getpos() returns a list [bufnum, lnum, col, off]
-	-- when lnum == 0 mark is not set
-	local pos = vim.fn.getpos("`" .. char)
-	if pos[2] == 0 then
-		print("Mark " .. char .. " is not set")
-		return false
-	end
-	local special_marks = {
-		["'"] = true, ["\""] = true, ["^"] = true, ["."] = true,
-		["["] = true, ["]"] = true, ["<"] = true, [">"] = true
-	}
-	if char >= 'a' and char <= 'z' or
-	   char >= 'A' and char <= 'Z' or
-	   special_marks[char] then
-		return true
-	end
-	return false
+    -- Check legal characters
+    local is_legal = (char >= 'a' and char <= 'z') or
+                     (char >= 'A' and char <= 'Z') or
+                     (char >= '0' and char <= '9') or
+                     special_marks[char]
+    if not is_legal then
+        return false, "not a legal mark character."
+    end
+    -- Check if set
+    -- getpos() returns a list [bufnum, lnum, col, off]
+    -- when lnum == 0 the mark is not set
+    local pos = vim.fn.getpos("`" .. char)
+    if pos[2] == 0 then
+        return false, "not set."
+    end
+    return true
 end
 
 return M
--- .config/nvim/lua/modules/user_functions.lua
+++ .config/nvim/lua/modules/user_functions.lua
@@ -1,30 +1,35 @@
 local helper_functions = require("modules.helper_functions")
 
-M = {}
+local M = {}
 
 function M.jump_to_mark()
     while true do
-	    print("Mark: ")
-	    local key = vim.fn.nr2char(vim.fn.getchar())
-	    print("KEy = " .. key)
-	    if key == "" then
-		    print("Cancelled")
-		    return
-	    end
-	    if helper_functions.is_valid_mark(key) then
-		    vim.cmd.normal("`" .. key)
-		    print("Jumped to mark " .. key)
-		    return
-	    end
+        vim.api.nvim_echo({{"Mark <Esc> to cancel:", "Normal"}}, false, {})
+        local key = vim.fn.getchar(-1, { number = false })
+        if key == "\27" then
+            vim.api.nvim_echo({{"Cancelled.", "Normal"}}, false, {})
+            return
+        end
+        local valid, reason = helper_functions.is_valid_mark(key)
+        if valid then
+            vim.cmd.normal("`" .. key)
+            vim.api.nvim_echo({{"Jumped to mark " .. key, "Normal"}}, false, {})
+            return
+        else
+            if reason then
+                vim.api.nvim_echo({{"Mark '" .. key .. "' is " .. reason, "ErrorMsg"}, {" Press any key to continue...", "Comment"}}, false, {})
+            end
+            vim.fn.getchar(-1)
+        end
     end
 end
 
 function M.yank_into_register()
-	print("Select register to yank into: ")
+    vim.api.nvim_echo({{"Select register to yank into: ", "Normal"}}, false, {})
 end
 
 function M.paste_from_register()
-	print("Select register to paste from: ")
+    vim.api.nvim_echo({{"Select register to paste from: ", "Normal"}}, false, {})
 end
 
 return M
